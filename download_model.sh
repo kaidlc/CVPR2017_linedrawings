8c55613210ad62714979f18207622952598676a2
Edgar Simo-Serra <bobbens@gmail.com>
2026-06-17 19:26:09 +0900

    Fixed download link.

--- download_model.sh
+++ download_model.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # md5sum dd26e0e243259cce0f8d1cefacef34f6
-# sha1sum 
+# sha1sum
 FILENAME="cvpr2017_linedrawings.t7"
-FILEURL="http://hi.cs.waseda.ac.jp/~sasaki/data/cvpr2017_linedrawings.t7"
+FILEURL="http://esslab.jp/~ess/files/cvpr2017_linedrawings.t7"
 FILEMD5="7834cfac30dc10b7cec78bfc28989114"
 
 echo "Downloading the inpainting model (44M)..."
@@ -21,5 +21,5 @@ if [ "$CHECKSUM" != "$FILEMD5" ]; then
   echo "Integrity check failed. File is corrupt!"
   echo "Try running this script again and if it fails remove '$FILENAME' before trying again."
   exit 1
-fi 
+fi
 echo -e "Download finished successfully!"
